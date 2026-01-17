4cc8ffc9c11827aa27d13f9ed0d621605b2b1802
Tanish Deshmukh <119071827+Taniz-08@users.noreply.github.com>
2026-01-17 20:27:48 +0000

    data types

---
+++ 01-Basic/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict";  //treat all JS code as newer version
+
+// alert("Be Tanish Deshmukh - Full Stack Developer")
+// // alert(2+2) //we are using node js not browser
+
+console.log(3+3)
+console.log("Tanish Deshmukh") //code readability should be high
+
