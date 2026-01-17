137cd1152ff60b4fc06ad5f1469540521e2e7c99
Tanish Deshmukh <119071827+Taniz-08@users.noreply.github.com>
2026-01-17 20:28:08 +0000

    varaible

---
+++ 01-Basic/01_variables.js
@@ -0,0 +1,19 @@
+const accountID = 75071
+let accountEmail = "deshmukh@gmail.com"
+var accountPassword = "11111"
+accountCity = "Nagpur"
+let accountState
+
+//accountID = 2 // not allowed 
+// - mean comment in JS
+
+console.log(accountID)
+accountEmail ="tanish@gmail.com"
+accountPassword="33333"
+accountCity="Delhi"
+
+/* Prefare not to use var
+Beacause of issur in block scope and funnctional scope*/
+
+console.table([accountEmail,accountID,accountPassword,accountCity,accountState
+])
